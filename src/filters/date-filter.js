2748ba1dda6a98fdc310723574bbf2b83b83efff
Rob Lindsey <rob@roblindsey.com>
2020-03-23 17:07:19 -0400

    timezone for timestamps

--- src/filters/date-filter.js
+++ src/filters/date-filter.js
@@ -1,7 +1,8 @@
-const moment = require('moment');
+const moment = require('moment-timezone');
 
 module.exports = function dateFilter(value) {
 	const dateObject = moment.unix(Number(value));
+	const tzone = dateObject.tz('America/New_York');
 
-	return `${dateObject.format('MMMM D')}, ${dateObject.format('h:mma')}`;
+	return `${dateObject.format('MMMM D')}, ${tzone.format('h:mma z')}`;
 };
